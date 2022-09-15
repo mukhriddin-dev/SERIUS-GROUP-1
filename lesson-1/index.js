c948f76dc936f03dafdab0b44dfcf7c318376bf1
mukhriddin-dev <khodievm@gmail.com>
2022-09-15 18:11:23 +0500

    lesson-1

---
+++ lesson-1/index.js
@@ -0,0 +1,83 @@
+"use strict";
+
+// a=10;
+// b=25;
+// a=667;
+
+// var fruits='apple';
+// fruits='limon'
+// var fruits="apelsin";
+
+// let fruits='apple';
+// fruits='limon'
+// fruits='peach'
+// fruits='ananas'
+// let fruits='banana'; no correcrt
+
+
+// const fruits='apple';
+// fruits='aplesin'
+// const fruits='limon';
+
+// =============== 2 ===============////
+
+
+// math operators  + , - , * , / , % , ** , 
+// ++ INCR , -- DECR
+
+// > , < , >= , <= , == v , === v , t, ! 
+// is not  
+
+// let a=10;
+// let b=8;
+
+// a=a+b;
+// a=a*b;
+// a=a/b;
+// a=a%b;
+// a=a**b;
+
+// a+=b;
+// a-=b;
+// a*=b;
+// a**=b;
+// a%=b;
+// a/=b;
+
+
+
+// console.log(a);
+
+// =========== 3 ===========
+
+let light='transparent';
+
+if(light==='red'){
+
+   console.log('GO');
+
+}else if(light==='yellow'){
+
+   console.log('warning');
+
+}else if(light="transparent"){
+
+  console.log(' SVET YO\'Q ');
+  
+}else{
+
+   console.log('STOP or WARNING')
+}
+
+// console.log(light==='red');
+
+
+
+
+
+
+
+
+
+
+
